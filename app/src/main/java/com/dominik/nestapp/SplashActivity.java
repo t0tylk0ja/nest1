6a69ae97a72cd2032072e228c980c16701d99d26
Dominik <dominikmalek91@gmail.com>
2018-06-19 00:37:26 +0200

    20180619_splash

---
+++ app/src/main/java/com/dominik/nestapp/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.dominik.nestapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TextView splashTV = (TextView) findViewById(R.id.splashName);
+        TextView splashIntro = (TextView) findViewById(R.id.splashIntro);
+        splashTV.setTextColor(Color.WHITE);
+        splashIntro.setTextColor(Color.WHITE);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
